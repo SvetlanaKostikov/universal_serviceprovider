4c148635901966f7aa301d6e7115938ffdd61c7f
sveta <svetlanakostikova4@gmail.com>
2019-04-10 14:27:50 +0300

    First commit-heroku

--- src/main/java/telran/java23/serviceprivder/controller/HerokuController.java
+++ src/main/java/telran/java23/serviceprivder/controller/HerokuController.java
@@ -16,8 +16,7 @@ public class HerokuController {
 
         return "<h1>ScheduleService</h1>\n" +
                 "<h4>v.1.1 from 01.03.2019</h4>\n" +
-                "<a href=\"https://pacific-sierra-61961.herokuapp.com/swagger-ui.html#/\"><h2>Welcome to Swagger...</h2></a>" +
-                "<a href=\"https://docs.google.com/document/d/e/2PACX-1vRowcDLW-J-mQaEEnz3emmuMuE4Q3GdK9h3TsvanvBCUtsm_tQ7UYt_H4IaPPe-MyywP6ymNzsWxRNb/pub\">" +
+                "<a href=\"https://universalserviceprovider.herokuapp.com/swagger-ui.html#/\"><h2>Welcome to Swagger...</h2></a>" +
                 "<h2>Api Documentation</h2></a>";
     }
 }
